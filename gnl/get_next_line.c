8a30e1463c9ef9593f3bcb96b76de84ff50438ed
adminvasheypomoiki <kek@cheburek.lol>
2018-12-26 00:03:29 +0300

    it's alive!

--- gnl/get_next_line.c
+++ gnl/get_next_line.c
@@ -1,42 +1,39 @@
 #include <stdio.h>
 #include "get_next_line.h"
-#include <locale.h>
-void free_a(char *a, char *b, char *c)
-{
-	free(a);
-	free(b);
-	free(c);
-	a = 0x0;
-	b = 0x0;
-	c = 0x0;
-}
 
-char *getString(char *cr_line, const char *redd, char *temp)
-{
-	temp = ft_strdup(cr_line);
-	free(cr_line);
-	cr_line = ft_strjoin(temp, redd);
-	free(temp);
-	return cr_line;
-}
 
-char *magic(const int fd, char **storage, char *cr_line, char *temp)
+void free_m( char **b, char **c)
 {
-	cr_line = ft_strdup(temp); // same
-	free(temp);
-	temp = ft_strjoin(storage[fd], cr_line); //cr_line +
-//	free(&storage[fd]); //fail reason
-	storage[fd] = ft_strdup(temp); // all with \n
-	free(temp);
-	return cr_line;
+    free(*b);
+    free(*c);
+    *b = 0x0;
+    *c = 0x0;
 }
-
-void init( char **strg, char **cr_line, char **redd)
+void init( char **strg, char **cr_line, char **redd, int *resp)
 {
 	*redd = ft_strnew(BUFF_SIZE);
 	*cr_line = ft_strnew(BUFF_SIZE);
 	if (*strg == NULL)
 		*strg = ft_strnew(1);
+	*resp = 1;
+}
+char * parse_and_cut(char **stor, char **cr_line, char **redd)
+{
+    char *temp;
+
+    temp = ft_strjoin(*cr_line, *redd);
+    free_m(redd, cr_line);
+    *cr_line = ft_strjoin(*stor, temp);
+    free_m(stor, &temp);
+    temp = ft_strcopy_until(*stor, '\n');
+    if (ft_strchr(*stor,'\n') != 0x0) {  // if \n exists cut it
+        *cr_line = ft_strdup(ft_strchr(*stor, '\n'));
+        free(*stor);
+        *stor = *cr_line;
+    }
+
+
+    return temp;
 }
 
 int get_next_line(const int fd, char **line)
@@ -47,38 +44,36 @@ int get_next_line(const int fd, char **line)
 	char *temp;
 	int response;
 
-	temp = 0x0;
 	if (fd == -1)
 		return -1;
-	init( &storage[fd], &cr_line, &redd);
-	while (ft_strchr(storage[fd], '\n') == 0x0 &&(response =
-			(int)read(fd, redd, BUFF_SIZE)) !=0 &&
-			ft_strchr(redd, 10) == 0x0)
-		cr_line = getString(cr_line, redd, temp);
-	if (!response)
-		return 0;
-	temp = ft_strjoin(cr_line, redd); // here stores what has been read
-	cr_line = magic(fd, storage, cr_line, temp);
-	if (ft_strchr(storage[fd], '\n') == 0x0)
-		temp = ft_strdup((storage[fd]));
-	else
-		temp = ft_strcopy_until(storage[fd], '\n');
-	*line = ft_strdup(temp);
-	free(temp);
-	temp = ft_strdup(ft_strchr(storage[fd], '\n') + 1);
-	storage[fd] = ft_strdup(temp);
-	free_a(cr_line, redd, temp);
-	return 1;
+    init(&storage[fd], &cr_line, &redd, &response); //todo strg = strnew 1 byte, others are set on buffsize
+	if (ft_strchr(storage[fd],'\n') == 0x0)  // just reading until \n is met
+        while (ft_strchr(storage[fd], '\n') == 0x0 && (response =(int) read(fd, redd, BUFF_SIZE)) != 0 &&
+               ft_strchr(redd, 10) == 0x0)
+        {
+            temp = ft_strdup(cr_line);
+            free(cr_line);
+            cr_line = ft_strjoin(temp, redd);
+            free(temp);
+        }
+
+    *line = parse_and_cut(&storage[fd], &cr_line,&redd);
+    printf("%s",storage[fd]);
+    if (response == 0)
+        return 0;
+    return 1;
 }
 
 int main(int argc, char **argv)
 {
 	int fd = open(argv[1], O_RDONLY);
 	char *read_b = ft_strnew(1024 * 1024);
-	while (get_next_line(fd, & read_b))
-	{
-		printf("%s\n", read_b);
-	}
+//	while (get_next_line(fd, & read_b))
+//	{
+//		printf("%s\n", read_b);
+//	}
+	get_next_line(fd, &read_b);
 //	get_next_line(fd, &read_b);
-//	printf("%s\n", read_b);
+//	get_next_line(fd, &read_b);
+//   printf("%s\n", read_b);
 }
--- gnl/get_next_line.h
+++ gnl/get_next_line.h
@@ -4,7 +4,7 @@
 
 #ifndef GET_NEXT_LINE_GET_NEXT_LINE_H
 #define GET_NEXT_LINE_GET_NEXT_LINE_H
-#define BUFF_SIZE 1000
+#define BUFF_SIZE 2
 #include <fcntl.h>
 #include <printf.h>
 #include "unistd.h"
