7086dda0baeb761bd77bacad5746c174d3b9c8dd
adminvasheypomoiki <kek@cheburek.lol>
2018-12-27 20:08:55 +0300

    cleanup

--- main.c
+++
@@ -1,9 +0,0 @@
-//
-// Created by Quenton hightower Hettinger on 2018-12-26.
-//
-
-#include <fcntl.h>
-#include "libft.h"
-#include <printf.h>
-#include "get_next_line.h"
-
--- sources/ft_is_null_ptr.c
+++
@@ -1,8 +0,0 @@
-#include "libft.h"
-
-int ft_is_null_ptr(void *ptr)
-{
-	if (ptr ==NULL)
-		return (1);
-	return (0);
-}
