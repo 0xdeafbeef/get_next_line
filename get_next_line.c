554197f10e4b5627da5e18b54a37706a5497638d
adminvasheypomoiki <kek@cheburek.lol>
2019-01-10 18:50:01 +0300

    2 tests falling still

--- get_next_line.c
+++ get_next_line.c
@@ -13,88 +13,61 @@
 #include <printf.h>
 #include "get_next_line.h"
 
-void free_m(char **b, char **c)
+void concat(char *crnt,char **storage,char **temp,ssize_t resp)
 {
-	free(*b);
-	free(*c);
-	*b = 0x0;
-	*c = 0x0;
-}
-
-void init(char **strg, char **cr_line, char **redd, int *resp)
-{
-	*redd = ft_strnew(BUFF_SIZE);
-	*cr_line = ft_strnew(BUFF_SIZE);
-	if (*strg == 0x0)
-		*strg = ft_strnew(0);
-	*resp = 1;
-}
-
-char *parse_and_cut(char **stor, char **cr_line, char **redd)
-{
-	char *temp;
-//	if (ft_strchr(*stor, '\n') == 0x0)
-//		return ft_strjoin(*cr_line, *redd);
-	if (*redd != 0x0)
-	{
-		temp = ft_strjoin(*cr_line, *redd); //here we joines redd and buff
-		*cr_line = ft_strjoin(*stor, temp);// and here
-		free_m(stor, &temp);//+
-		*stor = ft_strdup(*cr_line);
-		free(*cr_line);
-	} else
-	{
-		temp = ft_strdup(*cr_line);
-	}
-
-	if (ft_strchr(*stor, '\n') != 0x0)
-	{
-		temp = ft_strcopy_until(*stor, '\n');
-		*cr_line = ft_strsub(ft_strchr(*stor, '\n'), 1,
-							 ft_strlen(ft_strchr(*stor, '\n')));
-		free(*stor);
-		*stor = ft_strdup(*cr_line);
-	}
-
-	free(*cr_line);
-	return (temp);
-}
-
-char *read_logics(char **cr_line, char **redd, char **temp)
-{
-	if (!*temp)
-		free(*temp);
-	*temp = ft_strdup(*cr_line);
-	free(*cr_line);
-	*cr_line = ft_strjoin(*temp, *redd);
+	crnt[resp] =0;
+	*temp = *storage;
+	*storage = ft_strjoin(*storage,crnt);
 	free(*temp);
-	ft_bzero(*redd, BUFF_SIZE);
-	return (*cr_line);
 }
 
-int get_next_line(const int fd, char **line)
+int		get_next_line(const int fd, char **line)
 {
-	static char *storage[590432];
-	char *cr_line;
-	char *redd;
-	char *temp;
-	int response;
+	ssize_t		resp;
+	char		crnt[BUFF_SIZE + 1];
+	static char	*str[MAX_LINUX_KERNEL_FD_COUNT];
+	int			len;
+	char		*tmp;
 
-	if (fd == -1)
+	if (fd < 0 || (!str[fd] && !(str[fd] = ft_strnew(1))))
 		return (-1);
-	init(&storage[fd], &cr_line, &redd, &response);
-	if (ft_strchr(storage[fd], '\n') == 0x0)
-		while ((response = (int) read(fd, redd, BUFF_SIZE)) != 0 &&
-			   ft_strchr(redd, 10) == 0x0)
-			cr_line = read_logics(&cr_line, &redd, &temp);
-	if (response == 0)
-	{
-		free(redd);
-		redd = 0x0;
-		*line = parse_and_cut(&storage[fd], &cr_line, &redd);
-		free(storage[fd]);
-		return (0);
-	}
-	*line = parse_and_cut(&storage[fd], &cr_line, &redd);
-	return (1);
+	while (!ft_strchr(str[fd], '\n') && (resp = read(fd, crnt,BUFF_SIZE)) > 0)
+		concat(crnt,&str[fd],&tmp,resp);
+	if (resp == -1 || !*(tmp = str[fd]))
+		return (resp == -1 ? -1 : 0);
+	if ((len = ft_strchr(str[fd], '\n') != 0x0))
+		*line = ft_strsub(str[fd], 0, ft_strchr(str[fd], '\n') - str[fd]);
+	else
+		*line = ft_strdup(str[fd]);
+	str[fd] = ft_strsub(str[fd], (unsigned int)(ft_strlen(*line) + len),
+						(size_t)(ft_strlen(str[fd]) - (ft_strlen(*line) + len)));
+	ft_strdel(&tmp);
+	return (!(!str[fd] && !ft_strnlen(*line,0xFFFFFFFF-1)));
 }
+
+int main()
+{
+
+
+	char 	*line;
+	int		out;
+	int		p[2];
+	char 	*str;
+	int		len = 50;
+
+	str = (char *)malloc(1000 * 1000);
+	*str = '\0';
+	while (len--)
+		strcat(str, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in leo dignissim, gravida leo id, imperdiet urna. Aliquam magna nunc, maximus quis eleifend et, scelerisque non dolor. Suspendisse augue augue, tempus");
+	out = dup(1);
+	pipe(p);
+	dup2(p[1], 1);
+
+	if (str)
+		write(1, str, strlen(str));
+	close(p[1]);
+	dup2(out, 1);
+	get_next_line(p[0], &line);
+	printf("%d",strcmp(line, str) == 0);
+}
+
