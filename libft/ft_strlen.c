003eccf00f95e97a3ab0714743a353a97d29ceb6
adminvasheypomoiki <kek@cheburek.lol>
2019-01-19 22:59:21 +0300

    upd

--- libft/ft_strlen.c
+++ libft/ft_strlen.c
@@ -6,18 +6,51 @@
 /*   By: qhetting <qhetting@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/02 17:28:07 by qhetting          #+#    #+#             */
-/*   Updated: 2018/12/02 17:28:33 by qhetting         ###   ########.fr       */
+/*   Updated: 2019/01/19 21:09:31 by qhetting         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include "strlen.h"
 
-size_t	ft_strlen(const char *str)
+unsigned long int			g_longword;
+unsigned long int			g_himagic;
+unsigned long int			g_lomagic;
+
+static void					inner(const char *char_ptr,
+		const unsigned long **longword_ptr, unsigned long *himagic,
+		unsigned long *lomagic)
 {
-	size_t i;
+	(*longword_ptr) = (unsigned long int *)char_ptr;
+	(*himagic) = 0x80808080L;
+	(*lomagic) = 0x01010101L;
+	(*himagic) = (((*himagic) << 16) << 16) | (*himagic);
+	(*lomagic) = (((*lomagic) << 16) << 16) | (*lomagic);
+}
 
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
+size_t						ft_strlen(const char *str)
+{
+	const char				*char_ptr;
+	const unsigned long	int	*longword_ptr;
+	const char				*cp;
+
+	char_ptr = str - 1;
+	while (((unsigned long int)char_ptr++ & (sizeof(g_longword) - 1)) != 0)
+	{
+		if (*char_ptr == '\0')
+			return (char_ptr - str);
+	}
+	inner(char_ptr, &longword_ptr, &g_himagic, &g_lomagic);
+	while (1)
+	{
+		g_longword = *longword_ptr++;
+		if (((g_longword - g_lomagic) & ~g_longword & g_himagic) != 0)
+		{
+			cp = (const char *)(longword_ptr - 1);
+			ARR;
+			ARR1;
+			ARR2;
+			ARR3;
+		}
+	}
 }
