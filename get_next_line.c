4d1220587494ec557ca2cacc3606e3cf403fb043
adminvasheypomoiki <kek@cheburek.lol>
2019-01-19 22:58:14 +0300

    upd

--- get_next_line.c
+++ get_next_line.c
@@ -1,73 +1,58 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: qhetting <qhetting@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/12/27 19:05:16 by qhetting          #+#    #+#             */
-/*   Updated: 2019/01/02 19:20:45 by odm3n            ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
 
-#include <printf.h>
 #include "get_next_line.h"
 
-void concat(char *crnt,char **storage,char **temp,ssize_t resp)
+REDD;
+
+char		*send_result(char *str, char **line)
 {
-	crnt[resp] =0;
-	*temp = *storage;
-	*storage = ft_strjoin(*storage,crnt);
-	free(*temp);
+	size_t	i;
+	char	*del;
+	char	*tmp;
+
+	i = 0;
+	tmp = ft_strnew(0);
+	while (str[i] != '\n' && str[i] != '\0')
+		i++;
+	del = tmp;
+	tmp = ft_strsub(str, (unsigned int)(i + 1), ft_strlen(&str[i]));
+	(*line) = ft_strsub(str, 0, (size_t)i);
+	ft_strdel(&del);
+	ft_strdel(&str);
+	return (tmp);
 }
 
-int		get_next_line(const int fd, char **line)
+int			READ
 {
-	ssize_t		resp;
-	char		crnt[BUFF_SIZE + 1];
-	static char	*str[MAX_LINUX_KERNEL_FD_COUNT];
-	int			len;
+	READ_BODY;
+	READ_BODY1;
+	READ_BODY2;
+
+	return (!ft_strchr(buf, '\n'));
+}
+
+int			get_next_line(const int fd, char **line)
+{
+	FRST;
+	static char	*str[MAX_UNIX_KERNEL_FD_COUNT];
 	char		*tmp;
 
-	if (fd < 0 || (!str[fd] && !(str[fd] = ft_strnew(1))))
+	if (fd < 0 || !line || read(fd, buf, 0) < 0)
+	{
+		CLEAR;
 		return (-1);
-	while (!ft_strchr(str[fd], '\n') && (resp = read(fd, crnt,BUFF_SIZE)) > 0)
-		concat(crnt,&str[fd],&tmp,resp);
-	if (resp == -1 || !*(tmp = str[fd]))
-		return (resp == -1 ? -1 : 0);
-	if ((len = ft_strchr(str[fd], '\n') != 0x0))
-		*line = ft_strsub(str[fd], 0, ft_strchr(str[fd], '\n') - str[fd]);
-	else
-		*line = ft_strdup(str[fd]);
-	str[fd] = ft_strsub(str[fd], (unsigned int)(ft_strlen(*line) + len),
-						(size_t)(ft_strlen(str[fd]) - (ft_strlen(*line) + len)));
-	ft_strdel(&tmp);
-	return (!(!str[fd] && !ft_strnlen(*line,0xFFFFFFFF-1)));
+	}
+	if (str[fd] == NULL)
+		str[fd] = ft_strnew(0);
+	if (!ft_strchr(str[fd], '\n'))
+	{
+		while ((RREDD = (int)read(fd, buf, BUFF_SIZE)) > 0 && READLER)
+			;
+		CLEAR;
+	}
+	if (*str[fd] != '\0')
+	{
+		str[fd] = send_result(str[fd], line);
+		return (1);
+	}
+	return (0);
 }
-
-int main()
-{
-
-
-	char 	*line;
-	int		out;
-	int		p[2];
-	char 	*str;
-	int		len = 50;
-
-	str = (char *)malloc(1000 * 1000);
-	*str = '\0';
-	while (len--)
-		strcat(str, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in leo dignissim, gravida leo id, imperdiet urna. Aliquam magna nunc, maximus quis eleifend et, scelerisque non dolor. Suspendisse augue augue, tempus");
-	out = dup(1);
-	pipe(p);
-	dup2(p[1], 1);
-
-	if (str)
-		write(1, str, strlen(str));
-	close(p[1]);
-	dup2(out, 1);
-	get_next_line(p[0], &line);
-	printf("%d",strcmp(line, str) == 0);
-}
-
--- get_next_line.h
+++ get_next_line.h
@@ -1,15 +1,46 @@
-//
-// Created by Quenton hightower Hettinger on 2018-12-07.
-//
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: qhetting <qhetting@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/01/15 20:26:33 by qhetting          #+#    #+#             */
+/*   Updated: 2019/01/19 22:25:10 by qhetting         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#define MAX_LINUX_KERNEL_FD_COUNT 590432
-#define BUFF_SIZE 64
-#include <fcntl.h>
-#include <unistd.h>
-#include "libft/libft.h"
+# define MAX_UNIX_KERNEL_FD_COUNT 590432
+# define BUFF_SIZE 50000
+# include <fcntl.h>
+# include <unistd.h>
+# include "libft/libft.h"
+# if BUFF_SIZE > 8388608
+#  define REDD int redd;
+#  define FRST char		*buf; buf = ft_strnew(BUFF_SIZE);
+#  define CLEAR ft_strdel(&buf)
+#  define READLER readler(&buf, &str[fd], &tmp)
+#  define READ readler(char **buf, char **str, char **tmp)
+#  define READ_BODY *tmp = ft_strdup(*str); ft_strdel(str);
+#  define READ_BODY1 *str=ft_strjoin(*tmp,*buf);ft_strdel(tmp);
+#  define READ_BODY2 ft_bzero(*buf,BUFF_SIZE);
+# define RREDD redd
+# else
+#  define REDD int redd;
+#  define FRST char		buf[BUFF_SIZE];
+#  define CLEAR *line = *line + 0;
+#  define INN char **str,char **tmp,int fd,int redd)
+#  define READ readler(char buf[BUFF_SIZE], INN
+#  define READLER readler(buf, &str, tmp, fd, redd)
+#  define READ_BODY tmp = &str[fd];
+#  define READ_BODY1 buf[redd] = '\0';
+#  define READ_BODY2 	*str[fd] = ft_strjoin(*tmp, buf); ft_strdel(tmp);
+# define RREDD redd
+
+# endif
 
 int		get_next_line(const int fd, char **line);
 #endif
